e93e2f50d01c363b924b069f77bae6f456fd7994
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2017-08-08 14:44:25 -0700

    Add Message model and database table
    Adds a very basic database table for 'Messages', and an application
    model to access and traverse them.

--- application/models.py
+++ application/models.py
@@ -1,9 +1,22 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session,sessionmaker,relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    ForeignKey,
+    DateTime,
+    Sequence,
+    Float
+)
 from index import db, bcrypt
 
 
 class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    email = db.Column(db.String(255), unique=True)
+    __tablename__ = "user"
+    id       = db.Column(db.Integer(), primary_key = True)
+    email    = db.Column(db.String(255), unique    = True)
     password = db.Column(db.String(255))
 
     def __init__(self, email, password):
@@ -22,3 +35,30 @@ class User(db.Model):
             return user
         else:
             return None
+
+class Message(db.Model):
+    __tablename__ = "message"
+    id            = db.Column(db.Integer(), primary_key = True)
+    event         = db.Column(db.Integer())
+    text          = db.Column(db.String(1024))
+    author        = db.Column(db.Integer,ForeignKey('user.id'))
+    timestamp     = db.Column(db.Integer())
+    parent        = db.Column(db.Integer,ForeignKey('message.id'))
+
+#    responses    = relationship('Message',
+#                        backref=backref('parent', remote_side=[id])
+#                    )
+
+    def __init__(self, event, text, author, timestamp, parent):
+        self.event = event
+        self.text = text
+        self.author = author
+        self.timestamp = timestamp
+        self.parent = parent
+
+    def get_responses():
+        return User.query.filter_by(parent=self.id)
+
+    @staticmethod
+    def get_for_event(event):
+        return User.query.filter_by(event=event.id,parent=null)
