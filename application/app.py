77cccc6e148ca953b57800faaaed1f40bb37dfb8
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2017-09-06 18:35:54 -0700

    Add "shutdown" endpoint for development
    Because the development server has to run as a multi-threaded app in
    order to server the webhooks now, it's really hard to shut it down when
    done working with it. This little snippet adds an endpoint which you can
    post to and shut it down.
    
        http post :5000/api/shutdown
    
    Should go without saying that this is just a hack for development and
    needs to be removed before this ever goes to production.

--- application/app.py
+++ application/app.py
@@ -263,5 +263,16 @@ def create_event():
 
     return get_events()
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route("/api/shutdown", methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down'
+
 if __name__ == '__main__':
     socketio.run(app)
