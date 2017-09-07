36e910e1b3a6332bafc68d2229dc4b90f1205508
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2017-09-07 13:33:21 -0700

    Show unread count in badges for events and messages
    Also adds a basic nav footer with badge notification when unread
    messages come in to one of these locations.

--- static/src/components/Events.js
+++ static/src/components/Events.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+
 import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import Badge from 'material-ui/Badge';
+
 import * as eventActionCreators from '../actions/events';
 
 function mapStateToProps(state) {
@@ -11,6 +14,7 @@ function mapStateToProps(state) {
         isFetching: state.events.isFetching,
         loaded: state.events.loaded,
         events: state.events.events,
+        messages: state.messages.messages,
     };
 }
 
@@ -27,17 +31,21 @@ class Events extends React.Component { // eslint-disable-line react/prefer-state
     }
 
     render() {
-        const { events } = this.props;
+        const { events, messages } = this.props;
         return (
             <div className="col-md-8">
                 <h1>{ events.length } New Events</h1>
                 <Divider inset={false} />
                 <List> 
                     { events.map( event => {
+                        const newMessages = messages.filter( m => m.event = event.id && m.unread ).length 
                         return (
                            <ListItem key={ event.id }
                                 primaryText={
                                     <Link to={`/events/${event.id}`}>{event.name}</Link>
+                                }
+                                secondaryText={
+                                    newMessages ? <Badge badgeContent={newMessages} primary={true} /> : null
                                 }
                             />
                         ) }
