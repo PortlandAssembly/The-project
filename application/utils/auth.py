871823d93fb1ffdbc29ad69281f43a7246f2769e
Brennan Lujan <berlun@gmail.com>
2017-09-08 19:13:14 -0400

    Initial add of Twilio request validator

--- application/utils/auth.py
+++ application/utils/auth.py
@@ -1,8 +1,10 @@
 from functools import wraps
-from flask import request, g, jsonify
+from flask import request, g, jsonify, abort, current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired, BadSignature
 from index import app
+from twilio.request_validator import RequestValidator
+import os
 
 TWO_WEEKS = 1209600
 
@@ -39,3 +41,26 @@ def requires_auth(f):
         return jsonify(message="Authentication is required to access this resource"), 401
 
     return decorated
+
+
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid (or if DEBUG is True)
+        # and return a 403 error if it's not
+        if request_valid or current_app.debug:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
